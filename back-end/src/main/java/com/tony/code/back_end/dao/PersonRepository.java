e4ed2e8c9415fbf929d746063d748ffcbbcd2442
avierra28 <tvierra30@outlook.com>
2024-06-06 11:36:16 -0400

    Adding backend connectivity.

--- back-end/src/main/java/com/tony/code/back_end/dao/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tony.code.back_end.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tony.code.back_end.entities.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
